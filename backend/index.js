c76302a1d918dd35f5d14389e0716b396659f8ae
Aditya Gulalia <adityagulalia50@gmail.com>
2026-05-08 10:03:00 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./db');
-require('dotenv').config();
+require('dotenv').config({ path: __dirname + '/.env' });
 
 const app = express();
 app.use(cors({
